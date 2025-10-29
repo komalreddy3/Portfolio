2678cc7760d9f6a6a1c9ee8a16f998f5d82925ea
unknown <komal@appzoy.com>
2025-10-29 16:08:54 +0530

    fix(!): few changes for github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Projects from './components/ProjectsPage/ProjectsPage';
