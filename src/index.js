7ed9a967a8dea6e305383ca58e69fb9030f66028
Jayanth Reddy Guttapally <koukuntlakomalreddy3@gmail.com>
2026-01-29 23:46:30 +0530

    fix(git add .): refresh route changes - switch to hashrouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css';
-
+import { HashRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+    <HashRouter >
       <App />
-    </BrowserRouter>
+      {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>
 );
